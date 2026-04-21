a62d564a9e90082b25214e1fa1f731e0d021839a
DipishaLama-git <dipisha701@gmail.com>
2026-04-21 22:16:35 +0545

    image is added and Navbar path

--- client/cloudbridge/src/pages/Home.jsx
+++ client/cloudbridge/src/pages/Home.jsx
@@ -1,34 +1,38 @@
 import StepCard from "../components/cloud/StepCard.jsx";
-
+import Navbar from '../components/layout/Navbar.jsx'
 const Home = () => {
   return (
-    <div className="p-10 bg-gray-50 min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-        <StepCard
-          step="01"
-          title="Configure"
-          description="Select your CPU, RAM, and storage requirements."
-          icon="⚙️"
-        />
-        <StepCard
-          step="02"
-          title="Quote"
-          description="Get an instant, transparent monthly cost estimate."
-          icon="📊"
-        />
-        <StepCard
-          step="03"
-          title="Approve"
-          description="Review and approve your configuration before deployment."
-          icon="✅"
-        />
-        <StepCard
-          step="04"
-          title="Provision"
-          description="Watch your infrastructure come to life automatically."
-          icon="⚡"
-        />
-      </div>
+    // <div className="p-10 bg-gray-50 min-h-screen">
+    //   <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+    //     <StepCard
+    //       step="01"
+    //       title="Configure"
+    //       description="Select your CPU, RAM, and storage requirements."
+    //       icon="⚙️"
+    //     />
+    //     <StepCard
+    //       step="02"
+    //       title="Quote"
+    //       description="Get an instant, transparent monthly cost estimate."
+    //       icon="📊"
+    //     />
+    //     <StepCard
+    //       step="03"
+    //       title="Approve"
+    //       description="Review and approve your configuration before deployment."
+    //       icon="✅"
+    //     />
+    //     <StepCard
+    //       step="04"
+    //       title="Provision"
+    //       description="Watch your infrastructure come to life automatically."
+    //       icon="⚡"
+    //     />
+    //   </div>
+    // </div>
+
+    <div>
+      <Navbar/>
     </div>
   );
 };
