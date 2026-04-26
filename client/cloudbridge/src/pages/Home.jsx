465e6365f9fbfcf78434a5d0861fc76438bf7307
Bipin Maharjan <bipinmrzn@gmail.com>
2026-04-26 16:14:37 +0545

    Adding CTASection component for pricing section

--- client/cloudbridge/src/pages/Home.jsx
+++ client/cloudbridge/src/pages/Home.jsx
@@ -1,30 +1,19 @@
 import React from "react";
-import FeatureSection from "../components/cloud/FeatureSection.jsx";
-// import Input from '../components/common/Input.jsx';
+
 import Navbar from '../components/layout/Navbar.jsx';
 import Footer from '../components/layout/Footer.jsx';
+import FeatureSection from '../components/cloud/FeatureSection.jsx';
+import CTASection from "../components/CTASection.jsx";
 import { Link } from "react-router-dom";
-// import GridBackground from "../components/common/GridBackground.jsx";
-import Logo from "../assets/Cloud_Bridge.svg";
-// import Button from "../components/common/Button.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "../pages/auth/Login.jsx";
-// import Signup from "../pages/auth/Signup.jsx";
 const Home = () => {
   return (
-    <div>
-      {/* <Logo />
-      <GridBackground />
-      <Button/>
-      */}
-      {/* <Login />
-      <Signup /> */}
-      <Navbar />
-      <FeatureSection />
-      {/* <Input /> */}
-     
-      <Footer/>
-    </div>
-  );
+      <div>
+        <Navbar />
+        <FeatureSection />
+        <CTASection />
+        <Footer />
+      </div>
+    );
 };
 export default Home;
